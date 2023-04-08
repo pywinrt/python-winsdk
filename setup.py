aaf0a59037d80f110d6c4460252c5946b23f4d95
David Lechner <david@pybricks.com>
2023-04-08 14:00:51 -0500

    setup.py: drop cmake_args
    The build tools automatically select Release and we don't need to force
    a specific compiler.

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     python_requires=">=3.7",
     packages=find_namespace_packages(where=("pywinrt")),
     package_dir={"": "pywinrt"}, 
-    cmake_args=['-DCMAKE_BUILD_TYPE=Release', '-DCMAKE_C_COMPILER=cl', '-DCMAKE_CXX_COMPILER=cl'],
     # recursive glob (**) doesn't seem to work here
     package_data={"winsdk": ["py.typed", "*.pyi", "*/*.pyi", "*/*/*.pyi", "*/*/*/*.pyi", "*/*/*/*/*.pyi"]},
 )
